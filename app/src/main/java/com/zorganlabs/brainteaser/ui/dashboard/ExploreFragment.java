e3b83a9200933b92baf3719a379271408cd2e3b1
Mukunda Bhattarai <mkdbht@gmail.com>
2021-12-16 00:26:47 -0500

    Added horizontal scroll

--- app/src/main/java/com/zorganlabs/brainteaser/ui/dashboard/ExploreFragment.java
+++ app/src/main/java/com/zorganlabs/brainteaser/ui/dashboard/ExploreFragment.java
@@ -9,20 +9,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.zorganlabs.brainteaser.databinding.FragmentDashboardBinding;
+import com.zorganlabs.brainteaser.databinding.FragmentExploreBinding;
 
-public class DashboardFragment extends Fragment {
+public class ExploreFragment extends Fragment {
 
-    FragmentDashboardBinding binding;
+    FragmentExploreBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentExploreBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        textView.setText("This is Dashboard");
         return root;
     }
 
